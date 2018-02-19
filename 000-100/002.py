f7737c1b2a67915291ed3bdcf6529794903eacf1
Ikumi Ichikawa <ichikawa@hopf.sys.es.osaka-u.ac.jp>
2018-02-20 07:36:41 +0900

    [add]002の解答を作成

---
+++ 000-100/002.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def main():
+    sum = 0
+    prev_f = 1
+    f = 2
+    while f <= 4e+6:
+        if f%2 == 0:
+            sum += f
+        temp = f
+        f += prev_f
+        prev_f = temp
+    print(sum)
+
+
+
+if __name__ == '__main__':
+    main()
