fa4586ec1131ed398bb7133bc4fd13a66c67b48f
Ikumi Ichikawa <ichikawa@hopf.sys.es.osaka-u.ac.jp>
2018-03-14 18:24:59 +0900

    [追加]002の解法を追加

--- 001-100/001-010/002.py
+++ 001-100/001-010/002.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# I coded
 def main():
     sum = 0
     prev_f = 1
@@ -12,6 +13,21 @@ def main():
         f += prev_f
         prev_f = temp
     print(sum)
+
+
+# code which calculate sum of sequence E(n)=4*E(n-1)+E(n-2)
+def main2():
+    num = 4e+6
+    a = 2
+    b = 8
+    sum = a
+    while b <= num:
+        sum += b
+        temp = b
+        b = 4*temp + a
+        a = temp
+    print(sum)
+
 
 if __name__ == '__main__':
     main()
