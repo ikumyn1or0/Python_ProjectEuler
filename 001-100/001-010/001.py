ed0d6625d39000747c3ef83bd96f781bba653b4a
Ikumi Ichikawa <ichikawa@hopf.sys.es.osaka-u.ac.jp>
2018-03-14 18:14:39 +0900

    [追加]001のプログラムを修正および解法を追加

--- 001-100/001-010/001.py
+++ 001-100/001-010/001.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# I coded
 def main():
     sum = 0
     for n in range(1000):
-        if n%3 == 0:
+        if n%3 == 0 or n%5 == 0:
             sum += n
-        if n%5 == 0:
-            sum += n
-        if n%15 == 0:
-            sum -= n
     print(sum)
 
+
+# code which can calculate O(1)
+def main2():
+    num = 1000 - 1
+    sum_to = lambda n: (n*(n+1))//2
+    result = 3*sum_to(num//3) + 5*sum_to(num//5) - 15*sum_to(num//15)
+    print(result)
+
+
 if __name__ == '__main__':
-    main()
+    main2()
